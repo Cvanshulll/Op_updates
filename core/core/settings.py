32373ca0f972e2c09402b254d97cabe269536150
Vanshul Choudhary <cvanshul09@gmail.com>
2023-09-03 15:36:51 +0530

    okay

--- core/core/settings.py
+++ core/core/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h&k43pmzp#vqzit&(3y(@m8==*29fbswnzti#)kkzdegky_xu4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'X2Ob57dvf5eo0YJE04Kn',
+        'HOST': 'containers-us-west-191.railway.app',
+        'PORT': '7204',
     }
 }
 
@@ -117,6 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'public/static')
 ]
@@ -131,3 +136,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
